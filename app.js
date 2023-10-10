7137c51caf02618e86e9d9e394e39dd65b9215e6
flash0412 <Shota.Sasaki2@ibm.com>
2023-10-10 15:58:31 +0900

    package追加

--- app.js
+++ app.js
@@ -1,1 +1,64 @@
-console.log("Hello");
+// mysqlモジュールを読み込む
+const mysql = require('mysql');
+
+// MySQLに接続するための設定
+const connection = mysql.createConnection({
+  host: 'localhost', // ホスト名
+  user: 'root', // ユーザー名
+  password: 'password', // パスワード
+  database: 'test' // データベース名
+});
+
+// MySQLに接続する
+connection.connect((err) => {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+  console.log('connected as id ' + connection.threadId);
+});
+
+// userテーブルからidとpwを取得する
+connection.query('SELECT id, pw FROM user', (err, rows, fields) => {
+  if (err) {
+    console.error('error querying: ' + err.stack);
+    return;
+  }
+  console.log('query results: ', rows);
+
+  // ユーザーからidとpwを入力させる
+  const readline = require('readline');
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
+  rl.question('Please enter your id: ', (answer1) => {
+    rl.question('Please enter your password: ', (answer2) => {
+      // 入力されたidとpwがuserテーブルの値と一致しているかチェックする
+      let match = false;
+      for (let row of rows) {
+        if (row.id === answer1 && row.pw === answer2) {
+          match = true;
+          break;
+        }
+      }
+      // 一致していたら200を返す
+      if (match) {
+        console.log(200);
+      } else {
+        console.log('Invalid id or password');
+      }
+      rl.close();
+    });
+  });
+});
+
+// MySQLから切断する
+connection.end((err) => {
+  if (err) {
+    console.error('error ending: ' + err.stack);
+    return;
+  }
+  console.log('connection end');
+});
